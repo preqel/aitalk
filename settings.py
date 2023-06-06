0e387960243e2fb8f50ff1fab7570a4d50f5bf57
Akang <15068728687@163.com>
2023-06-06 15:50:13 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 # SECRET_KEY（flask项目密钥,不用管,也用不到）
 SECRET_KEY = "asghnjadfbtywyn"
 
-# openAi api key
+# openAi api key 
 OPENAI_API_KEY = "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"
 
 # openAi 官方 api
